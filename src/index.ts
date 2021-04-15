7578d9fed074c226e9ea21e370ddaf03ac071ed0
Veselin Romić <omegavesko@gmail.com>
2021-04-15 22:09:08 +0200

    feat: JSDoc and RequestDetails interface

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,51 @@
 import * as blake3 from 'blake3';
 import { formatISO } from 'date-fns';
 
+interface RequestDetails {
+  domain?: string;
+  ip?: string;
+  userAgent?: string;
+}
+
 const hash = (input: blake3.HashInput): string =>
   blake3.hash(input).toString('hex');
 
+/**
+ * Get a standard anonymous user ID (hash(salt + domain + ip + user_agent)),
+ * from a request, like the one used by Plausible Analytics.
+ *
+ * @see {@link https://plausible.io/data-policy#how-we-count-unique-users-without-cookies}
+ *
+ * @param salt A salt. You should rotate this at least once a day,
+ *             and delete old salts as soon as you're done using them.
+ *             This ensures that you can't track users long-term.
+ * @param request Parameters extracted from your HTTP request.
+ */
 export const getAnonymousUserId = (
   salt: string,
-  domain: string,
-  ip: string,
-  userAgent: string
+  request: RequestDetails
 ): string => {
-  return hash(`${salt}|${domain}|${ip}|${userAgent}`);
+  return hash(
+    `${salt}|${request.domain ?? ''}|${request.ip ?? ''}|${request.userAgent ??
+      ''}`
+  );
 };
 
+/**
+ * Get an anonymous user ID from a request, using a modified (and slightly less secure)
+ * variant of the algorithm used by used by Plausible Analytics. This variant is
+ * optimized for apps that can't reliably keep state (e.g. serverless functions).
+ * Instead of a salt that changes on a regular basis, you only need to provide a
+ * secret that's always the same (and thus can be defined as e.g. an environment variable).
+ *
+ * @param secret Your secret.
+ * @param request Parameters extracted from your HTTP request.
+ */
 export const getAnonymousUserIdWithSecret = (
   secret: string,
-  domain: string,
-  ip: string,
-  userAgent: string
+  request: RequestDetails
 ): string =>
   getAnonymousUserId(
     hash(`${secret}/${formatISO(new Date(), { representation: 'date' })}`),
-    domain,
-    ip,
-    userAgent
+    request
   );
--- test/index.test.ts
+++ test/index.test.ts
@@ -7,17 +7,20 @@ jest
 
 test('getAnonymousUserId', () => {
   expect(
-    getAnonymousUserId('salt', 'test.test', '1.1.1.1', 'test/1.0')
+    getAnonymousUserId('salt', {
+      domain: 'test.test',
+      ip: '1.1.1.1',
+      userAgent: 'test/1.0',
+    })
   ).toEqual('596837e254826733d2078f28beb170c7487c35685dfdbd7d60fe73623a257ef7');
 });
 
 test('getAnonymousUserIdWithSecret', () => {
   expect(
-    getAnonymousUserIdWithSecret(
-      'test_secret',
-      'test.test',
-      '1.1.1.1',
-      'test/1.0'
-    )
+    getAnonymousUserIdWithSecret('test_secret', {
+      domain: 'test.test',
+      ip: '1.1.1.1',
+      userAgent: 'test/1.0',
+    })
   ).toEqual('aa322ee77d60c096af65b37d7f73a1a2df6252dcf2d4a5a6464c93ca40bece78');
 });
